2802f7b7ce0f360f094551d9102b4580d698b152
Asami Sakoda <asamisakoda@Asamis-MacBook-Pro.local>
2022-12-07 22:34:33 +0900

    Added bg v1  7/12/22

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 // import Hero from './components/Hero'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Resume from './components/Resume'
-import MyNumbers from './components/MyNumbers'
-import Portfolio from './components/Portfolio'
-import Technologies from './components/Technologies'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Resume from "./components/Resume";
+import MyNumbers from "./components/MyNumbers";
+import Portfolio from "./components/Portfolio";
+import Technologies from "./components/Technologies";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function MyApp() {
-  return (
-    <div className="text-slate-900  bg-orange-50">
-    {/* <Hero/> */}
-    <Navbar/>
-    <Header/>
-    <Resume/>
-    <MyNumbers/>
-    <Portfolio/>
-    <Technologies/>
-    <Contact/>
-    <Footer/>
-    </div>
-  )
+	return (
+		<div className="text-slate-900  bg-orange-50 overflow-hidden">
+			{/* <Hero/> */}
+			<Navbar />
+			<Header />
+			<Resume />
+			<MyNumbers />
+			<Portfolio />
+			<Technologies />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,72 +1,83 @@
+import {Hidden} from "@mui/material";
 import React from "react";
 import MockupAnimated from "./Animation";
 import Cta from "./Cta";
 
-const Header = () => {
+function Header() {
 	return (
-		<header className="flex md:flex-nowrap justify-center align-middle md:py-56 lg:py-48 md:-mt-24 lg:w-[1400px] w-100 m-auto">
-			<div className="space-y-6 p-4 mt-8 md:w-[60vw] w-full">
-				<p className="text-pink-600 text-lg md:text-left text-center">
-					Hey, I'm Pedro Elton
-				</p>
-				<h1 className="md:text-5xl text-3xl font-bold subpixel-antialiased text-center md:text-left">
-					Front-end Developer <br /> & UI/UX Designer
-				</h1>
-				<p className="text-zinc-600 md:w-[480px] leading-relaxed text-lg font-light text-center md:text-left">
-					My experience in the past 3 years were focused on web design
-					and UI/UX Design. As a front-end web developer I’m an
-					Entry-level/Junior on ReactJs, JavaScript, jQuery, NodeJs
-					and Express.
-				</p>
-				<div className="w-full flex flex-col lg:flex-row lg:space-y-0 space-y-4 justify-center md:justify-start items-center">
-					<Cta
-						text="Download my CV"
-						link="https://pedroelton.com/wp-content/uploads/2022/12/CV-Pedro-Elton-Developer-ptbr.pdf"
-					/>
-					<a
-						href="#contact"
-						className="">
-						<button className="px-20 my-8 md:my-0 underline underline-offset-2 text-lg font-bold hover:text-indigo-500 transition-all duration-300">
-							Contact me
-						</button>
-					</a>
+		<section
+			className="w-screen h-auto"
+			style={{
+				backgroundImage:
+					'url("https://pedroelton.com/wp-content/uploads/2022/12/linesHero.png")',
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "50%",
+				backgroundPosition: "top right",
+			}}>
+			<header className="flex md:flex-nowrap justify-center align-middle md:py-56 lg:py-48 md:-mt-24 lg:w-[1400px] w-100 m-auto">
+				<div className="space-y-6 p-4 mt-8 md:w-[60vw] w-full">
+					<p className="text-pink-600 text-lg md:text-left text-center">
+						Hey, I'm Pedro Elton
+					</p>
+					<h1 className="md:text-5xl text-3xl font-bold subpixel-antialiased text-center md:text-left">
+						Front-end Developer <br /> & UI/UX Designer
+					</h1>
+					<p className="text-zinc-600 md:w-[480px] leading-relaxed text-lg font-light text-center md:text-left">
+						My experience in the past 3 years were focused on web
+						design and UI/UX Design. As a front-end web developer
+						I’m an Entry-level/Junior on ReactJs, JavaScript,
+						jQuery, NodeJs and Express.
+					</p>
+					<div className="w-full flex flex-col lg:flex-row lg:space-y-0 space-y-4 justify-center md:justify-start items-center">
+						<Cta
+							text="Download my CV"
+							link="https://pedroelton.com/wp-content/uploads/2022/12/CV-Pedro-Elton-Developer-ptbr.pdf"
+						/>
+						<a
+							href="#contact"
+							className="">
+							<button className="px-20 my-8 md:my-0 underline underline-offset-2 text-lg font-bold hover:text-indigo-500 transition-all duration-300">
+								Contact me
+							</button>
+						</a>
+					</div>
+					<div className="md:w-[480px] w-100">
+						<h4 className="md:mt-24 mt-16 font-bold text-slate-900 mb-4">
+							Review:
+						</h4>
+						<q className="leading-relaxed text-zinc-600">
+							Pedro is one of the most incredible guys I ever met,
+							as a persojn I have no word to describe him, as a
+							professional, the best word to describe Pedro is
+							commitment. His commitment to accomplish tasks and
+							his strength to develop new skills are speechless
+						</q>
+						<br />
+						<cite>
+							{" "}
+							-{" "}
+							<span className="font-bold cursor-pointer text-slate-900">
+								Warley Godoi
+							</span>
+							, Hedge fund manager - Cascais, Portugal.
+						</cite>
+					</div>
 				</div>
-				<div className="md:w-[480px] w-100">
-					<h4 className="md:mt-24 mt-16 font-bold text-slate-900 mb-4">
-						Review:
-					</h4>
-					<q className="leading-relaxed text-zinc-600">
-						Pedro is one of the most incredible guys I ever met, as
-						a persojn I have no word to describe him, as a
-						professional, the best word to describe Pedro is
-						commitment. His commitment to accomplish tasks and his
-						strength to develop new skills are speechless
-					</q>
-					<br />
-					<cite>
-						{" "}
-						-{" "}
-						<span className="font-bold cursor-pointer text-slate-900">
-							Warley Godoi
-						</span>
-						, Hedge fund manager - Cascais, Portugal.
-					</cite>
+				{/* <div className="md:w-[100%] w-100 hidden md:inline">
+                <iframe
+                    src="https://my.spline.design/untitled-959027861bb08831444051746de775c7/"
+                    frameborder="0"
+                    width="100%"
+                    height="100%"
+                    className="-mt-32"
+                ></iframe> */}
+				<div className="md:w-[50vw] w-100 hidden md:inline">
+					{" "}
+					<MockupAnimated image="https://pedroelton.com/wp-content/uploads/2022/04/heroImg1-scaled.webp" />
 				</div>
-			</div>
-			{/* <div className="md:w-[100%] w-100 hidden md:inline">
-				<iframe
-					src="https://my.spline.design/untitled-959027861bb08831444051746de775c7/"
-					frameborder="0"
-					width="100%"
-					height="100%"
-					className="-mt-32"
-				></iframe> */}
-			<div className="md:w-[50vw] w-100 hidden md:inline">
-				{" "}
-				<MockupAnimated image="https://pedroelton.com/wp-content/uploads/2022/04/heroImg1-scaled.webp" />
-			</div>
-		</header>
+			</header>
+		</section>
 	);
-};
+}
 
 export default Header;
