c762e52322e441b2fc2defc4d5b2b732abcceee2
Pedro Elton <pedroelton@msn.com>
2023-02-08 18:20:46 +0900

    fixed cta  font weight

--- pages/components/CtaNewWindow.jsx
+++ pages/components/CtaNewWindow.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
-const CtaNewWindow = ({text, link}) => {
+const CtaNewWindow = ({ text, link }) => {
 	return (
 		<a
 			href={link}
 			target="_blank"
 			rel="noopener noreferrer">
-			<button className="bg-indigo-500 text-white text-center py-4 px-12 md:px-10 md:h-[62px] rounded-2xl hover:bg-indigo-600 transition-all duration-300 shadow-lg hover:shadow-indigo-500 flex flex-nowrap pt-5">
+			<button className="bg-indigo-500 text-white text-lg font-medium leading-tight text-center py-4 px-12 md:px-10 md:h-[62px] rounded-2xl hover:bg-indigo-600 transition-all duration-300 shadow-lg hover:shadow-indigo-500 flex flex-nowrap pt-5">
 				{text}
 				<OpenInNewIcon className="text-white flex ml-2" />
 			</button>
